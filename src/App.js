00875cc7d8b9263b0dac2ed3ada4be4b3f0bd1f8
Bryn <brynheath1@gmail.com>
2020-12-05 23:26:27 +0000

    app clean up & implemented functions to build the prime table
    3 functions have been added - a helper func to check prime numbers, a function to build inital X/Row, last function to build out all other rows

--- src/App.js
+++ src/App.js
@@ -1,4 +1,47 @@
 const App = () => {
+  const isPrime = (num) => {
+    for (let i = 2; i < num; i++) if (num % i === 0) return false;
+    return num > 1;
+  }; // helper function, check num === prime number
+
+  const hardCodeNum = 3;
+
+  const buildPrimes = (num) => {
+    const setupNumber = num + 1;
+    let arr = [0];
+    let i = 0;
+
+    do {
+      if (isPrime(i)) {
+        arr.push(i);
+      }
+      i++;
+    } while (arr.length < setupNumber);
+    return arr;
+  }; // func build the foundation array of prime numbers for the top row (X)
+
+  const initPrimeArray = buildPrimes(hardCodeNum);
+
+  const buildAllArrays = (array) => {
+    let setArray = [];
+    for (let r = 0; setArray.length < hardCodeNum + 1; r++) {
+      let eachArr = [];
+      for (let c = 0; eachArr.length < hardCodeNum + 1; c++) {
+        if (r === 0) {
+          eachArr.push(1 * array[c]);
+        } else {
+          eachArr.push(array[r] * array[c]);
+        }
+        eachArr[0] = array[r];
+      }
+      setArray.push(eachArr);
+    }
+    return setArray;
+  }; // func to build out the prime numbers X * Y
+
+  const tableArray = buildAllArrays(initPrimeArray);
+
+  console.log(tableArray);
   return (
     <div className="App">
       <p>loadedd</p>
